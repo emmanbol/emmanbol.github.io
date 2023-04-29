a0d9c2e728cd5c3e4397679dbc3311e0d21ee466
emmanbol <emmanbol@gmail.com>
2023-04-29 19:43:01 +0100

    moved present to folder, change later3

---
+++ static/js/11.7219b6e1.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpmozartfinance-frontend"]=this["webpackJsonpmozartfinance-frontend"]||[]).push([[11],{1064:function(n,t,e){"use strict";e.r(t);var a=e(1052);t.default=a.Line}}]);
+//# sourceMappingURL=11.7219b6e1.chunk.js.map
---
+++ static/js/13.6f44bd4c.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpmozartfinance-frontend"]=this["webpackJsonpmozartfinance-frontend"]||[]).push([[13],{910:function(n,o){}}]);
+//# sourceMappingURL=13.6f44bd4c.chunk.js.map
